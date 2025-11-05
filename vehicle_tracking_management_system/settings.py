de10b634e1022c8ef3ac815d14bf18caeb0a1dab
Shidhartha <turzo@omnilabes.com>
2025-11-05 20:22:50 +0600

    fix on settings

--- vehicle_tracking_management_system/settings.py
+++ vehicle_tracking_management_system/settings.py
@@ -26,7 +26,7 @@ load_dotenv(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-#l-k9qkr9njo4xh60=y-#-pe+lv-fm44ddzm#*mas7m3w0p=gy')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
